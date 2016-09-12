fc3c849f5fd693ab3afdb079bfe698d51ac1f846
vivianig <gnviviani@gmail.com>
2016-09-12 16:18:41 -0700

    ui

---
+++ src/rest/public/main.js
@@ -0,0 +1,85 @@
+$(function() {
+    $("#datasetAdd").click(function() {
+	var id = $("#datasetId").val();
+	var zip = $("#datasetZip").prop('files')[0];
+	var data = new FormData();
+	data.append("zip", zip)
+	$.ajax("/dataset/"+id,
+	       {
+		   type:"PUT",
+		   data: data,
+		   processData: false
+	       }).fail(function(e) {spawnErrorModal(e)});
+	      
+    });
+
+    $("#datasetRm").click(function() {
+	var id = $("#datasetId").val();
+	$.ajax("/dataset/"+ id, {type:"DELETE"}).fail(function(e) {spawnErrorModal(e)});
+	       
+
+    });
+    
+    $("#queryForm").submit(function(e) {
+	e.preventDefault();
+	var query = $("#query").val()
+	$.post("/query", JSON.parse(query), function(data) {
+	    if(data["render"] === "table") {
+		generateTable(data["result"]);
+	    }
+	}).fail(function(e) {spawnErrorModal(e)});
+    });
+
+    function generateTable(data) {
+	var columns = [];
+	Object.keys(data[0]).forEach(function(title) {
+	    columns.push({
+		head: title,
+		cl: "title",
+		html: function(d) {return d[title]}
+	    });
+	})
+	var container = d3.select("#render");
+	container.html("");
+	container.selectAll("*").remove();
+	var table = container.append("table").style("margin", "auto");
+	
+	table.append("thead").append("tr")
+	    .selectAll("th")
+	    .data(columns).enter()
+	    .append("th")
+	    .attr("class", function(d) {return d["cl"]})
+	    .text(function(d) {return d["head"]});
+
+	table.append("tbody")
+	    .selectAll("tr")
+	    .data(data).enter()
+	    .append("tr")
+	    .selectAll("td")
+	    .data(function(row, i) {
+		return columns.map(function(c) {
+		    // compute cell values for this specific row
+		    var cell = {};
+		    d3.keys(c).forEach(function(k) {
+			cell[k] = typeof c[k] == "function" ? c[k](row,i) : c[k];
+		    });
+		    return cell;
+		});
+	    }).enter()
+	    .append("td")
+	    .html(function(d) {return d["html"]})
+	    .attr("class", function(d) {return d["cl"]});
+    }
+
+    function spawnErrorModal(e) {
+	console.log(e)
+	$("#errorModal .modal-title").html(e.status);
+	$("#errorModal .modal-body p").html(e.statusText + "</br>"+e.responseText);
+	if($('#errorModal').is(':hidden')) {
+	    $("#errorModal").modal('show')	    
+	}
+
+    }
+});
+
+
