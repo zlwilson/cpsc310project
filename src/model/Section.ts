4d0ce33d1ad899f2192be7659430248916ef273b
pablo <pablo@dhcp-206-12-64-62.ubcsecure.wireless.ubc.ca>
2016-09-24 15:04:21 -0700

    added a Section interface and Course class in a model folder

---
+++ src/model/Section.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by pablo on 9/24/16.
+ */
+interface Section {
+    Title: string;
+    Section: string;
+    id: number;
+    Professor: string;
+    Audit: number;
+    Withdrew: number
+    Year: number;
+    Stddev: number;
+    Enrolled: number;
+    High: number;
+    Course: number;
+    Session: string;
+    Pass: number;
+    Fail: number;
+    Avg: number;
+    Campus: string;
+    Subject: string;
+}
