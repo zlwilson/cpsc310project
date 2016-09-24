88022e9f795a5972ebb05eeb379227444438afce
pablo <pablo@dhcp-206-12-64-62.ubcsecure.wireless.ubc.ca>
2016-09-24 15:05:40 -0700

    Updated Course data struct

--- src/model/Course.ts
+++ src/model/Course.ts
@@ -1,3 +1,20 @@
 /**
  * Created by pablo on 9/24/16.
  */
+export default class Course {
+
+    dept: string;
+    num: number;
+
+    result: {};
+    rank: number;
+    
+    createCourse(input: string): Course {
+        let course = new Course;
+        return course;
+    }
+
+    courseID(course: Course): string {
+        return course.dept+course.num;
+    }
+}
