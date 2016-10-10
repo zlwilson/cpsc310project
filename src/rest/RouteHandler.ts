13595fb2b4efdcd81172c82638d126d9a77f08c4
pablo <pablo@Pablo.local>
2016-10-09 21:39:05 -0700

    Can now check if an array is in /data

--- src/rest/RouteHandler.ts
+++ src/rest/RouteHandler.ts
@@ -73,34 +73,90 @@ export default class RouteHandler {
             let controller = new QueryController(datasets);
             let isValid = controller.isValid(query);
             let idList = controller.getId(query);
+            console.log('Z - the list of ids to check: ' + idList);
 
             if (isValid === true) {
                 var isPut:boolean;
                 var missedId: string[] = [];
-                for(var i = 0; i < idList.length; i++) {
-                    console.log('Z - checking if dataset ' + idList[i] + ' exists');
-                    // p is Promise<boolean>
-                    let idName = idList[i];
 
-                    let p = RouteHandler.datasetController.getDataset(idList[i]).then(function (result) {
-                        isPut = result;
-                        console.log('Z - isPut? ' + isPut);
-                        if (isPut === false) {
-                            missedId.push(idName);
-                            console.log('Z - missing id: ' + idName);
-                        }
-                        if (typeof missedId === "undefined" || missedId.length == 0) {
-                            let result = controller.query(query, idList[0]);
-                            res.json(200, result);
-                        }  else {
-                            console.log('Z - missing an id, about to throw 424');
-                            res.json(424, {missing: JSON.stringify(missedId)});
-                        }
-                    }).catch(function (err) {
-                        // res.json(424, {missing: JSON.stringify(missedId)});
-                        console.log('Z - this is the ERROR: ' + err);
-                    });
+                // for(var i = 0; i < idList.length; i++) {
+                //     console.log('Z - checking if dataset ' + idList[i] + ' exists');
+                //     // p is Promise<boolean>
+                //     let idName = idList[i];
+                //
+                //     let p = RouteHandler.datasetController.getDataset(idList[i]).then(function (result) {
+                //         isPut = result;
+                //         console.log('Z - isPut? ' + isPut);
+                //         if (isPut === false) {
+                //             missedId.push(idName);
+                //             console.log('Z - missing id: ' + idName);
+                //         }
+                //         if (typeof missedId === "undefined" || missedId.length == 0) {
+                //             let result = controller.query(query, idList[0]);
+                //             res.json(200, result);
+                //         }  else {
+                //             console.log('Z - missing an id, about to throw 424');
+                //             res.json(424, {missing: missedId});
+                //         }
+                //     }).catch(function (err) {
+                //         // res.json(424, {missing: JSON.stringify(missedId)});
+                //         console.log('Z - this is the ERROR: ' + err);
+                //     });
+                // }
+
+                // passing an array of id's to check
+                // let p = RouteHandler.datasetController.getDatasetArray(idList).then(function (result) {
+                    // result should be an array of strings
+                    // console.log('Z - the array of missing files is: ' + result);
+                    // if (typeof missedId === "undefined" || missedId.length == 0) {
+                    //     let result = controller.query(query, idList[0]);
+                    //     res.json(200, result);
+                    // }  else {
+                    //     console.log('Z - missing an id, about to throw 424');
+                    //     res.json(424, {missing: missedId});
+                    // }
+                // }).catch(function (err) {
+                //     console.log('Z - something when wrong creating the array of missing file names')
+                // });
+                var promisedArray: Promise<any>[] = [];
+
+                for (var e in idList) {
+                    let p = RouteHandler.datasetController.getDataset(idList[e]);
+                    promisedArray.push(p);
                 }
+
+                Promise.all(promisedArray).then(function (result) {
+                    for (var x in result) {
+                        if (!result[x]) {
+                            missedId.push(idList[x]);
+                        }
+                        console.log('Z - ' + idList[x] + ' exists: ' + result[x]);
+                    }
+                }).then(function () {
+                    if (typeof missedId === "undefined" || missedId.length == 0) {
+                        let result = controller.query(query, idList[0]);
+                        res.json(200, result);
+                    }  else {
+                        console.log('Z - missing an id, about to throw 424');
+                        res.json(424, {missing: missedId});
+                    }
+                });
+
+                // Promise.all(promisesArray).then(function (result) {
+                //     for (var r in result) {
+                //         if (!result[r]) {
+                //             missedId.push(idList[r]);
+                //         }
+                //     }
+                // }).then(function () {
+                //     if (typeof missedId === "undefined" || missedId.length == 0) {
+                //         let returnVal = controller.query(query, idList[0]);
+                //         res.json(200, returnVal);
+                //     }  else {
+                //         console.log('Z - missing an id, about to throw 424');
+                //         res.json(424, {missing: missedId});
+                //     }
+                // });
             } else {
                 res.json(400, {status: 'invalid query'});
             }
