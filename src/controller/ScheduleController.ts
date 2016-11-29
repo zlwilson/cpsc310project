855ae8e2dc77500a0d3bff0974754d2b1adadcf6
pablo <pablo@dhcp-206-12-65-36.ubcsecure.wireless.ubc.ca>
2016-11-29 12:16:02 -0800

    Added quality checker

--- src/controller/ScheduleController.ts
+++ src/controller/ScheduleController.ts
@@ -27,13 +27,13 @@ export default class ScheduleController {
     private schedule: Scheduled[] = [];
 
     private roomIsFree(room: Room, time: Time): boolean {
-        console.log('Z - in roomIsFree(), room: ' + room.name + ', time: ' + time.time + ' ' + time.days);
+        // console.log('Z - in roomIsFree(), room: ' + room.name + ', time: ' + time.time + ' ' + time.days);
 
         if (this.schedule.length == 0) {
             return true;
         }
         for (let s in this.schedule) {
-            console.log('Z - in roomisFree() loop, room: ' + this.schedule[s].Room.name + ', time: ' + this.schedule[s].time.time);
+            // console.log('Z - in roomisFree() loop, room: ' + this.schedule[s].Room.name + ', time: ' + this.schedule[s].time.time);
             if (this.schedule[s].Room.name == room.name && this.schedule[s].time.days == time.days && this.schedule[s].time.time == time.time) {
                 return false;
             }
@@ -43,12 +43,12 @@ export default class ScheduleController {
 
     private findTime(section: Section, possibleRooms: Room[], time: Time): Scheduled {
         let that = this;
-        console.log('Z - in findTime()...');
+        // console.log('Z - in findTime()...');
         var timeslot = new Scheduled();
         let flag: boolean = false;
 
         for (let r in possibleRooms) {
-            console.log('Z - in findTime(), is room free? ' + this.roomIsFree(possibleRooms[r], time));
+            // console.log('Z - in findTime(), is room free? ' + this.roomIsFree(possibleRooms[r], time));
             if (this.roomIsFree(possibleRooms[r], time)) {
 
                 timeslot.time = time;
@@ -58,19 +58,19 @@ export default class ScheduleController {
             }
         }
 
-        console.log('Z - in findTime(), did not find timeslot');
+        // console.log('Z - in findTime(), did not find timeslot');
 
         let newTime = time.getNext();
-        console.log('Z - reached end of possible rooms at current time, new time: ' + newTime.time + ' ' + newTime.days)
+        // console.log('Z - reached end of possible rooms at current time, new time: ' + newTime.time + ' ' + newTime.days)
 
         return that.findTime(section, possibleRooms, newTime);
     }
 
     private getRooms(size: number, rooms: Room[]): Room[] {
         // get all rooms large enough for a section, but not too large (2*size)
-        console.log('Z - in getRooms(), size = ' + size);
+        // console.log('Z - in getRooms(), size = ' + size);
 
-        console.log('Z - in getRooms(), rooms = ' + rooms.length);
+        // console.log('Z - in getRooms(), rooms = ' + rooms.length);
         let array: Room[] = [];
 
         for (let r in rooms) {
@@ -87,26 +87,36 @@ export default class ScheduleController {
             return obj1.Seats - obj2.Seats;
         });
 
-        console.log('Z - getRooms() array: ' + array.length);
+        // console.log('Z - getRooms() array: ' + array.length);
         return array;
     }
 
     public makeSchedule(rooms: Room[], sections: Section[]): Scheduled[] {
-        console.log('Z - in makeSchedule()...');
+        // console.log('Z - in makeSchedule()...');
         for (let i in sections) {
             let scheduledRoom = new Scheduled();
             let time = new Time('MWF', 8);
 
             let possibleRooms = this.getRooms(sections[i].Size, rooms);
-            console.log('Z - in makeSchedule(), number of possible rooms: ' + possibleRooms.length);
+            // console.log('Z - in makeSchedule(), number of possible rooms: ' + possibleRooms.length);
 
             var timeslot = this.findTime(sections[i], possibleRooms, time);
 
-            console.log('Z - in makeSchedule(), number of timeslots: ' + this.schedule.length);
+            // console.log('Z - in makeSchedule(), number of timeslots: ' + this.schedule.length);
 
             this.schedule.push(timeslot);
         }
         return this.schedule;
+    }
+
+    public checkQuality(schedule: Scheduled[]): number {
+        let counter = 0;
+        for (let i in schedule) {
+            if (schedule[i].time.time > 17) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
 }
--- test/ScheduleControllerSpec.ts
+++ test/ScheduleControllerSpec.ts
@@ -30,7 +30,7 @@ describe('ScheduleController', function () {
             room.FullName = building;
             room.ShortName = name;
             room.Number = (100+i).toString();
-            room.name = name.concat((100+i).toString());
+            room.name = name.concat('_').concat((100+i).toString());
             room.Address = add;
             room.latitude = lat;
             room.longitude = lon;
@@ -94,7 +94,7 @@ describe('ScheduleController', function () {
         return expect(class1.time.time).to.equal(8);
     });
 
-    it.only('Should be able to create schedule (with some conflicts)', function () {
+    it('Should be able to create schedule (with some conflicts)', function () {
         Log.test('Creating room array');
         let roomArray: Room[] = [];
         let building = 'Building';
@@ -112,7 +112,7 @@ describe('ScheduleController', function () {
             room.FullName = building;
             room.ShortName = name;
             room.Number = (100+i).toString();
-            room.name = name.concat((100+i).toString());
+            room.name = name.concat('_').concat((100+i).toString());
             room.Address = add;
             room.latitude = lat;
             room.longitude = lon;
@@ -174,5 +174,93 @@ describe('ScheduleController', function () {
         let class2 = schedule[1];
 
         return expect(class2.time.time).to.equal(9);
+    });
+
+    it.only('Should be able to check quality of perfect schedule', function () {
+        Log.test('Creating room array');
+        let roomArray: Room[] = [];
+        let building = 'Building';
+        let name = 'BLD';
+        let add = '123 Main St.';
+        let lat = 49;
+        let lon = 41;
+        let seats = 20;
+        let type = 'Small';
+        let furniture = 'Movable';
+        let href = 'www.ubc.ca';
+        let i = 1;
+        while (i < 8) {
+            let room = new Room();
+            room.FullName = building;
+            room.ShortName = name;
+            room.Number = (100+i).toString();
+            room.name = name.concat('_').concat((100+i).toString());
+            room.Address = add;
+            room.latitude = lat;
+            room.longitude = lon;
+            room.Seats = seats*i;
+            room.Type = type;
+            room.Furniture = furniture;
+            room.href = href;
+            roomArray.push(room);
+            i++;
+        }
+
+        Log.test('Creating section array');
+        let sectionArray: Section[] = [];
+        let title = 'new course';
+        let prof = 'Teacher';
+        let audit = 0;
+        let withdrew = 2;
+        let year = 2014;
+        let stddev = 5.2;
+        let high = 95;
+        let course = 100;
+        let session = 'w';
+        let avg = 83;
+        let campus = 'ubc';
+        let subject = 'cpsc';
+        let j = 1;
+        while (j < 8) {
+            let newSection = new Section();
+            newSection.Title = title;
+            newSection.Professor = prof;
+            newSection.Audit = audit;
+            newSection.Withdrew = withdrew;
+            newSection.Year = year;
+            newSection.Stddev = stddev;
+            newSection.High = high;
+            newSection.Course = course+j.toString();
+            newSection.Session = session;
+            newSection.Avg = avg;
+            newSection.Campus = campus;
+            newSection.Subject = subject
+
+            newSection.Section = j.toString().concat('a');
+            newSection.id = '24'.concat(j.toString())
+            newSection.Enrolled = 18;
+            newSection.Pass = newSection.Enrolled*.8;
+            newSection.Fail = newSection.Enrolled*.2;
+            newSection.Size = newSection.Enrolled;
+
+            sectionArray.push(newSection);
+            j++;
+        }
+
+        console.log('Z - room array size: ' + roomArray.length);
+        console.log('Z - section array size: ' + sectionArray.length);
+
+        let controller = new ScheduleController();
+        let schedule = controller.makeSchedule(roomArray, sectionArray);
+        let quality = controller.checkQuality(schedule);
+
+        console.log('Z - Schedule - quality score: ' + quality);
+        for (let i in schedule) {
+            console.log(' ' + schedule[i].Section.Subject + schedule[i].Section.Course);
+            console.log('   in ' + schedule[i].Room.name);
+            console.log('   at ' + schedule[i].time.time + 'oclock on ' + schedule[i].time.days);
+        }
+
+        return expect(quality).to.equal(0);
     });
 });
